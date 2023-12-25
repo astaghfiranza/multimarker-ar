c8ae9a3f5f2f44cfc749614a35f78a74e87ae978
astaghfiranza <80092571+astaghfiranza@users.noreply.github.com>
2023-12-25 11:48:52 +0700

    add object js

---
+++ js/multiMarkersObject.js
@@ -0,0 +1,66 @@
+//Multi Markers WebAR-AR.js and Aframe - Playing the Archive - Connected Environment CASA-UCL
+
+//Global Variable
+var markersURLArray = [];
+var markersNameArray = [];
+
+AFRAME.registerComponent("markers_start", {
+  init: function () {
+    console.log("Add markers to the scene");
+
+    var sceneEl = document.querySelector("a-scene");
+
+    //list of the markers
+    for (var i = 1; i < 19; i++) {
+      //marker
+      var url = "resources/markers/pattern-Individual_Blocks-" + i + ".patt";
+      markersURLArray.push(url);
+      //object
+      var objectUrl = "resources/object/object_" + i + ".jpg";
+      markersNameArray.push(objectUrl);
+      //console.log(url);
+    }
+
+    for (var k = 0; k < 18; k++) {
+      var markerEl = document.createElement("a-marker");
+      markerEl.setAttribute("type", "pattern");
+      markerEl.setAttribute("url", markersURLArray[k]);
+      markerEl.setAttribute("id", markersNameArray[k]);
+
+      markerEl.setAttribute("registerevents", "");
+      sceneEl.appendChild(markerEl);
+
+      //Adding text to each marker
+      var objectEl = document.createElement("a-entity");
+      //var objectEl = document.createElement("a-assets");
+
+      objectEl.setAttribute("id", "image");
+      objectEl.setAttribute("image", {
+        src: markersNameArray[k],
+        width: "3",
+        height: "1.5",
+      });
+      objectEl.object3D.position.set(0, 0.7, 0);
+      objectEl.object3D.rotation.set(-90, 0, 0);
+
+      markerEl.appendChild(objectEl);
+    }
+  },
+});
+
+//Detect marker found and lost
+AFRAME.registerComponent("registerevents", {
+  init: function () {
+    const marker = this.el;
+
+    marker.addEventListener("markerFound", () => {
+      var markerId = marker.id;
+      console.log("Marker Found: ", markerId);
+    });
+
+    marker.addEventListener("markerLost", () => {
+      var markerId = marker.id;
+      console.log("Marker Lost: ", markerId);
+    });
+  },
+});
